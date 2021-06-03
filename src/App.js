e3dda7c0f11da0f814565a4ab24feb5624dfcce3
mdjunaid14 <mdjunaid14@outlook.com>
2021-06-03 16:25:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import About from './components/About'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+        {
+          id: 1,
+          text: "Buy Books"
+        },
+        {
+          id: 2,
+          text: "Meeting"
+        },
+        {
+          id: 3,
+          text: "Office work"
+        },
+        {
+          id: 4,
+          text: "Get groceries"
+        }
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  return (
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/AddTask.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text) {
+      alert('Please add a task')
+      return
+    }
+
+    onAdd({ text })
+
+    setText('')
+  }
+
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+      <div className='form-control'>
+        <label>Task</label>
+        <input
+          type='text'
+          placeholder='Add Task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+
+      <input type='submit' value='Save Task' className='btn btn-block' />
+    </form>
+  )
+}
+
+export default AddTask
